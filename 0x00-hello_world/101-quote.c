bd10390deaa6d60a79763d364faee245aca1dc66
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-03-16 05:59:22 -0700

    Modify Task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,18 @@
-#include <unistd.h>
+#include<unistd.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 1 (Error)
- */
+ * Description: print a quote using write function
+ *    ssize_t write(int fd, const void *buf, size_t count);
+ *
+ * Return: This time we return an error 1.
+*/
+
 int main(void)
 {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-        return (1);
+	write(1, quo, 59);
+	return (1);
 }
