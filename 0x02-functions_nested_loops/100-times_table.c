b3870a233e8814ceb8a13f32e7950f8bacc5331b
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-03-21 05:09:53 -0700

    Modify Task 13

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,9 +12,9 @@ void print_times_table(int n)
 	if (n > 15 || n < 0)
 		return;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			k = j * i;
 			if (j == 0)
