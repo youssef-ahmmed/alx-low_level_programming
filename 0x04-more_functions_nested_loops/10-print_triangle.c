dd4931f25973da8c40bf243347210da24cf72844
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-03-24 14:43:17 +0200

    Add Task 10 and 11

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle
+ * @size: size of triangle
+ * Description: prints a triangle
+ * Return: void
+*/
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = size; i > 0; i--)
+	{
+		for (j = 0; j < i - 1; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 0; k <= size - i; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: FizzBuzz program that prints the numbers from 1 to 100.
+ * But for multiples of 3 print Fizz instead of the number
+ * And for multiples of 5 print Buzz.
+ * For numbers which are multiples of both 3 and 5 print FizzBuzz.
+ * Return: Always 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
