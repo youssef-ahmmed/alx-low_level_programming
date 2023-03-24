4fee8727b1e3a9c87bbb63633f717778c753ff7a
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-03-24 15:37:22 +0200

    Modify last task

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -16,10 +16,10 @@ void print_number(int n)
 		num = -n;
 	}
 
-	if (n / 10)
+	if (num / 10)
 	{
-		print_number(n / 10);
+		print_number(num / 10);
 	}
 
-	_putchar((n % 10) + '0');
+	_putchar((num % 10) + '0');
 }
