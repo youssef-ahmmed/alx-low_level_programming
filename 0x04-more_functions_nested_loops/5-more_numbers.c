bea4c1db0a6f4b7ec93e301cfd1b66f6007f96bd
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-03-24 13:56:54 +0200

    Modify Task 6

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,9 +7,9 @@ void more_numbers(void)
 {
 	int num, i, j;
 
-	for (i = 0; i < 15; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			num = j;
 			if (j > 9)
@@ -17,7 +17,7 @@ void more_numbers(void)
 				_putchar(1 + 48);
 				num = j % 10;
 			}
-			_putchar(j + '0');
+			_putchar(num + '0');
 		}
 		_putchar('\n');
 	}
