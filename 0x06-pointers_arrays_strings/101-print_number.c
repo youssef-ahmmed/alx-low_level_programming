af87a3a05e732f2d0aa8f0ef9db688891c25a0a0
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-04-10 15:16:53 +0200

    Add task 10

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the number to be printed
+*/
+void print_number(int n)
+{
+	unsigned int num;
+
+	num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+
+	if (num / 10 != 0)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
