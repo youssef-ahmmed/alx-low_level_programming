007d4a416241b8472972b682088244f62246f2f6
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-04-13 00:06:13 +0200

    Add task 3 and 4

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * alloc_grid - allocates memory for an array
+ * @width: width of the array
+ * @height: height of the array
+ * Return: pointer to the array
+*/
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = (int **) malloc(sizeof(int *) * height);
+	
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *) malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+
+	return (grid);
+}
