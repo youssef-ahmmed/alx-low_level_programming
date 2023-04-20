8f3dcccacb358c2780c75d2ee08ca4d88eb02a84
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-04-20 17:09:58 +0200

    edit betty style of task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		char *str = va_arg(args, char *);
-		
+
 		if (str == NULL)
 			printf("(nil)");
 		else if (str != NULL)
