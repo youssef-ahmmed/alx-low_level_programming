3bd0684ff43cf09d0a772bb6daef6e9635e0daa1
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-04-28 04:56:48 +0200

    Edit task 4 again

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /**
- * free_list - frees a list_t list
+ * free_list - frees a linked list
  * @head: head of the list
  */
 void free_list(list_t *head)
 {
 	list_t *curr;
 
-	while (curr)
+	while (head)
 	{
 		curr = head->next;
 		free(head->str);
