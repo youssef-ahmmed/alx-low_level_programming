b9000d6c3a344af761c51ce4f84af34c55da6fc9
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-04-28 03:49:55 +0200

    Add task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: head of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *curr;
+
+	while (head != NULL)
+	{
+		curr = head->next;
+		free(head->str);
+		free(head);
+		head = curr;
+	}
+}
