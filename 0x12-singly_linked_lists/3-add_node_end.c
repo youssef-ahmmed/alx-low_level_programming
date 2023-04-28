1567c023e68d48c5ca7be3905fd963ff73724b38
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-04-28 03:31:29 +0200

    Edit betty style of task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -21,7 +21,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-    new_node->str = strdup(str);
+	new_node->str = strdup(str);
 	new_node->next = NULL;
 
 	if (*head == NULL)
