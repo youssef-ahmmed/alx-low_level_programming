2b0760b2a5abef2a177636fa154e111c78d59591
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-05-04 15:36:11 +0200

    Edit task 0 again :(

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,41 +1,25 @@
 #include "main.h"
 
 /**
- * check_binary - check if the char contain only 1's and 0's
- * @b: the number in string format
- * Return: 1 if it's 0's and 1's, 0 otherwise
- */
-int check_binary(const char *b)
-{
-	int len = 0;
-
-	while (b[len])
-	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
-		len++;
-	}
-
-	return (1);
-}
-
-/**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: the number in string format
+ * binary_to_uint - convert a binary number to an unsigned int
+ * @b: number in string format
  * Return: converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int len = strlen(b);
-	unsigned int u_int = 0, pow;
+	unsigned int u_int, pow;
+	int len;
 
 	if (b == NULL)
 		return (0);
 
-	if (!check_binary(b))
-		return (0);
+	for (len = 0; b[len]; len++)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+	}
 
-	for (len--, pow = 1; len >= 0; len--, pow *= 2)
+	for (pow = 1, u_int = 0, len--; len >= 0; len--, pow *= 2)
 	{
 		if (b[len] == '1')
 			u_int += pow;
