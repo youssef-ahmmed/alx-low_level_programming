5eb24640ef6e395155b46a9198e5f4a6fa232bc8
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-05-09 01:59:04 +0200

    Add task 4

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,8 +5,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <elf.h>
+
 
 #define BUFFER_SIZE 1024
 
@@ -14,10 +17,24 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
+/* cp.c */
 char *new_buffer(char *file);
 void close_file(int file_descriptor);
 void check_argument(int argc);
 void check_open_read_write(int fd_from, int r, char *file_from_name,
 		int fd_to, int w, char *file_to_name, char *buffer);
+
+/* elf_headers.c */
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 #endif /* MAIN_H */
