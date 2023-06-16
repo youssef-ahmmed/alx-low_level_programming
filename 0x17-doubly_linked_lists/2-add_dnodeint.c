02a24af9467c6c655a17a5bab319fd247def037e
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-06-16 06:33:36 +0300

    Edit betty style of task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -5,25 +5,26 @@
  * @head: pointer to the head of the list
  * @n: data to be inserted
  * Return: the address of the new element, or NULL if it failed
-*/
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
-    if (!new_node)
-        return (NULL);
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = *head;
+	if (!new_node)
+		return (NULL);
 
-    if (!head || !*head)
-    {
-        *head = new_node;
-        return (*head);
-    }
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
 
-    (*head)->prev = new_node;
-    *head = new_node;
+	if (!head || !*head)
+	{
+		*head = new_node;
+		return (*head);
+	}
 
-    return (*head);
+	(*head)->prev = new_node;
+	*head = new_node;
+
+	return (*head);
 }
