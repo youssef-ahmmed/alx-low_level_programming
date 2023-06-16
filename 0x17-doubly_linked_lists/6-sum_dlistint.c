4df4531454c8de42b428134454728d1b0fad9937
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-06-16 07:33:30 +0300

    Edit betty style of task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -4,20 +4,20 @@
  * sum_dlistint - sum of all the data (n) of a linked list
  * @head: pointer to the head of the list
  * Return: summation
-*/
+ */
 int sum_dlistint(dlistint_t *head)
 {
-    dlistint_t *current = head;
-    int sum = 0;
+	dlistint_t *current = head;
+	int sum = 0;
 
-    if (!head)
-        return (0);
-    
-    while (current != NULL)
-    {
-        sum += current->n;
-        current = current->next;
-    }
+	if (!head)
+		return (0);
 
-    return (sum);
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+
+	return (sum);
 }
