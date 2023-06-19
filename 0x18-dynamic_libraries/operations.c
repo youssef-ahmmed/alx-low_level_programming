18bb1601788914c6cf10a4ebfe662af6a4f04b72
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-06-19 21:24:51 +0300

    Add task 2

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,66 @@
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+/**
+ * add - adds two number
+ * @a: first number
+ * @b: second number
+ * Return: sum
+*/
+int add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * sub - subtracts two number
+ * @a: first number
+ * @b: second number
+ * Return: difference
+*/
+int sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * mul - multiplies two number
+ * @a: first number
+ * @b: second number
+ * Return: product
+*/
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * div - divides two number
+ * @a: first number
+ * @b: second number
+ * Return: quotient
+*/
+int div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+ * mod - modulo of two number
+ * @a: first number
+ * @b: second number
+ * Return: remainder
+*/
+int mod(int a, int b)
+{
+    return (a % b);
+}
+
+
+
+
+
+
