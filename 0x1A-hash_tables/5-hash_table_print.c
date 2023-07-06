4313b71ec9074452901a68d90d740384c841e384
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-07-06 22:25:44 +0300

    Add task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current_node;
+	int printed = 0;
+
+	printf("{");
+
+	if (!ht)
+	{
+		printf("}\n");
+		return;
+	}
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current_node = ht->array[i];
+		while (current_node)
+		{
+			if (printed)
+				printf(", ");
+			printf("'%s': '%s'", current_node->key, current_node->value);
+			printed++;
+			current_node = current_node->next;
+		}
+	}
+
+	printf("}\n");
+}
