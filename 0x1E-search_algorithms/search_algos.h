61f71a21cf678ae1583754f03c70b69df6a04291
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-10-10 06:44:47 +0300

    Add header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef SEARCH_ALGORITHMS_H
+#define SEARCH_ALGORITHMS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_search_range(int *array, int low, int high);
+int jump_search(int *array, size_t size, int value);
+size_t linear_search_for_jump(int *array, size_t start, size_t end, int value);
+int interpolation_search(int *array, size_t size, int value);
+
+#endif
